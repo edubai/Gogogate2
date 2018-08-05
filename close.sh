ca0eacdc297122031558b158610a26a4abcce24b
fserruto <fabian@serruto.net>
2018-08-05 23:10:05 +1000

    Update close.sh
    Added logic to check door state before sending open/close command

--- close.sh
+++ close.sh
@@ -1,4 +1,10 @@
 !/bin/bash
+retval=`curl -b gogocookie.txt http://gogogatelocalIP/isg/statusDoor.php?numdoor=1`
 
-curl -d 'login=userlogin&pass=userpassword&send-login=submit' -c gogocookie.txt
-curl -b gogocookie.txt http://gogogatelocalIP/isg/opendoor.php?numdoor=1
+if [ "$retval" == "OK" ]
+    then
+        curl -d 'login=userlogin&pass=userpassword&send-login=submit' -c gogocookie.txt http://gogogatelocalIP/index.php
+        curl -b gogocookie.txt http://gogogatelocalIP/isg/opendoor.php?numdoor=1
+    else
+        echo CLOSED
+fi
